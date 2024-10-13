8d7fae2943467f5aa3bfac584cdf6cd3724bfdb4
Levizor <levizorri@gmail.com>
2024-10-13 12:47:37 +0200

    hyprpaper

--- home-manager/wm/default.nix
+++ home-manager/wm/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hyprland.nix
     # ./wpaperd.nix
+    ./hyprpaper.nix
     ./wlogout.nix
   ];
 
---
+++ home-manager/wm/hyprpaper.nix
@@ -0,0 +1,16 @@
+{
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      splash = false;
+      preload = ["/home/levizor/Pictures/Wallpapers/moons.jpg"
+        "/home/levizor/Pictures/Wallpapers/mountains.jpg"
+      ];
+
+      wallpaper = [
+        "eDP-1, /home/levizor/Pictures/Wallpapers/moons.jpg"
+        "HDMI-A-1, /home/levizor/Pictures/Wallpapers/mountains.jpg"
+      ];
+    };
+  };
+}
