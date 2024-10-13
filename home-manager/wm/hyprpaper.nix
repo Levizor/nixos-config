20e563c3f23e7635120292ddbaefd21619b87178
Levizor <levizorri@gmail.com>
2024-10-13 13:05:22 +0200

    hyprpaper

--- home-manager/wm/hyprpaper.nix
+++ home-manager/wm/hyprpaper.nix
@@ -4,12 +4,12 @@
     settings = {
       splash = false;
       preload = ["/home/levizor/Pictures/Wallpapers/moons.jpg"
-        "/home/levizor/Pictures/Wallpapers/mountains.jpg"
+        # "/home/levizor/Pictures/Wallpapers/mountains.jpg"
       ];
 
       wallpaper = [
         "eDP-1, /home/levizor/Pictures/Wallpapers/moons.jpg"
-        "HDMI-A-1, /home/levizor/Pictures/Wallpapers/mountains.jpg"
+        "HDMI-A-1, /home/levizor/Pictures/Wallpapers/moons.jpg"
       ];
     };
   };
