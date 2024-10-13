5be64b6f3c56e30665f0943731d541ec5bd91d39
Levizor <levizorri@gmail.com>
2024-10-13 12:57:01 +0200

    hyprpaper

--- os/default.nix
+++ os/default.nix
@@ -148,6 +148,20 @@
      })
   ];
 
+  services.hyprpaper = {
+    enable = true;
+    settings = {
+      splash = false;
+      preload = ["/home/levizor/Pictures/Wallpapers/moons.jpg"
+        "/home/levizor/Pictures/Wallpapers/mountains.jpg"
+      ];
+
+      wallpaper = [
+        "eDP-1, /home/levizor/Pictures/Wallpapers/moons.jpg"
+        "HDMI-A-1, /home/levizor/Pictures/Wallpapers/mountains.jpg"
+      ];
+    };
+  };
 
 
 
