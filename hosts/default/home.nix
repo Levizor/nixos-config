a3b110566358202f20dc7bf94d87e049269fa7c9
Levizor <levizorri@gmail.com>
2024-10-22 21:15:00 +0200

    nord

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -3,7 +3,7 @@
   imports = [
     outputs.modules/home-manager
     inputs.home-manager.nixosModules.home-manager
-    outputs.modules/stylix
+    outputs.modules/stylix/nord.nix
     inputs.stylix.nixosModules.stylix
   ];
 }
