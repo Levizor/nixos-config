34119c11b2312b5c3e401ffeb07380ad977cd8f5
Levizor <levizorri@gmail.com>
2025-02-10 23:07:35 +0100

    oops

--- modules/stylix/cyberdream.nix
+++ modules/stylix/cyberdream.nix
@@ -4,7 +4,7 @@
 
     polarity = "dark";
     image = ./moons.jpg;
-    stylix.base16Scheme = builtins.fetchurl {
+    base16Scheme = builtins.fetchurl {
       url = "https://raw.githubusercontent.com/scottmckendry/cyberdream.nvim/main/extras/base16/cyberdream.yaml";
       sha256 = "1bfi479g7v5cz41d2s0lbjlqmfzaah68cj1065zzsqksx3n63znf";
     };
