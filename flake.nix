21603f24765f7afbfaf4cc9d7e21947156d23699
Levizor <levizorri@gmail.com>
2025-04-19 22:29:07 +0200

    make inputs cleaner

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix";
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs;"
+    };
 
     nixvim = {
       url = "github:Levizor/nixvim";
