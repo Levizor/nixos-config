ede1169578a07a193302648352871409d7d30c43
Levizor <levizorri@gmail.com>
2025-04-19 22:27:08 +0200

    make inputs cleaner

--- flake.nix
+++ flake.nix
@@ -2,27 +2,32 @@
   description = "NixOS Config";
 
   inputs = {
-    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stable.url = "github:nixos/nixpkgs/nixos-24.11";
+
     minimal-tmux = {
       url = "github:niksingh710/minimal-tmux-status";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    pipewire-screenaudio.url = "github:IceDBorn/pipewire-screenaudio";
-    stable.url = "github:nixos/nixpkgs/nixos-24.11";
+
     stylix.url = "github:danth/stylix";
+
     nixvim = {
       url = "github:Levizor/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
   outputs =
