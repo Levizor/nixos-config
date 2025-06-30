e8432998fca2b49e10c8472b589274ca5bcb2c7a
Levizor <levizorri@gmail.com>
2025-06-30 19:21:14 +0200

    cool wlogout style

--- modules/home-manager/wm/wlogout.nix
+++ modules/home-manager/wm/wlogout.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   programs.wlogout = {
     enable = true;
@@ -39,5 +40,79 @@
         keybind = "r";
       }
     ];
+    style =
+      let
+        wlogoutIcons = {
+          lock = "${pkgs.wlogout}/share/wlogout/icons/lock.png";
+          logout = "${pkgs.wlogout}/share/wlogout/icons/logout.png";
+          suspend = "${pkgs.wlogout}/share/wlogout/icons/suspend.png";
+          hibernate = "${pkgs.wlogout}/share/wlogout/icons/hibernate.png";
+          shutdown = "${pkgs.wlogout}/share/wlogout/icons/shutdown.png";
+          reboot = "${pkgs.wlogout}/share/wlogout/icons/reboot.png";
+        };
+      in
+      ''
+        * {
+        	background-image: none;
+        	box-shadow: none;
+        }
+
+        window {
+        	background-color: rgba(10, 10, 25, 0.92);
+        	padding: 20px;
+        }
+
+        /* Add spacing between buttons */
+        button {
+        	margin: 10px;
+        	padding: 20px;
+        	border-radius: 12px;
+        	border-color: #6C6CE5;
+        	text-decoration-color: #E0F7FA;
+        	color: #E0F7FA;
+        	background-color: transparent;
+        	border-style: solid;
+        	border-width: 2px;
+        	background-repeat: no-repeat;
+        	background-position: center;
+        	background-size: 25%;
+        	transition: all 0.2s ease-in-out;
+        }
+
+        /* Hover/active/focus effect with glow */
+        button:focus,
+        button:active,
+        button:hover {
+        	background-color: rgba(47, 25, 95, 0.3); /* Slight tint on hover */
+        	outline-style: none;
+        	box-shadow: 0 0 10px #8BE9FD, 0 0 20px #6C6CE5;
+        }
+
+        /* Button icons */
+        #lock {
+        	background-image: image(url("${wlogoutIcons.lock}"));
+        }
+
+        #logout {
+        	background-image: image(url("${wlogoutIcons.logout}"));
+        }
+
+        #suspend {
+        	background-image: image(url("${wlogoutIcons.suspend}"));
+        }
+
+        #hibernate {
+        	background-image: image(url("${wlogoutIcons.hibernate}"));
+        }
+
+        #shutdown {
+        	background-image: image(url("${wlogoutIcons.shutdown}"));
+        }
+
+        #reboot {
+        	background-image: image(url("${wlogoutIcons.reboot}"));
+        }
+      '';
   };
+
 }
