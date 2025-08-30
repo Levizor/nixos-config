f9af534876a847048e979a87a3de3cacec21cd02
Levizor <levizorri@gmail.com>
2025-08-30 14:12:02 +0200

    fix hyprlock

--- hosts/vps/configuration.nix
+++ hosts/vps/configuration.nix
@@ -41,10 +41,15 @@
     inherit (config) myopts;
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 
   services = {
     openssh.enable = true;
--- modules/home-manager/wm/hyprlock.nix
+++ modules/home-manager/wm/hyprlock.nix
@@ -16,6 +16,18 @@
         }
       ];
 
+      input-field = {
+        size = "200, 50";
+        position = "0, -20";
+        halign = "center";
+        valign = "center";
+        outer_color = "rgb(151515)";
+        inner_color = "rgb(200, 200, 200)";
+        font_color = "rgb(10, 10, 10)";
+        placeholder_text = "<i>Input Password...</i>";
+        rounding = -1;
+      };
+
     };
   };
 }
