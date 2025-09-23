b9d17ede30e0a6d3d1e28ed9e79880b78b81453a
Levizor <levizorri@gmail.com>
2025-09-24 00:28:06 +0200

    add noFirewall specialisation

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,7 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
+  specialisation."noFirewall".configuration = {
+    networking.firewall.enable = lib.mkForce false;
+  };
   networking = {
     hostName = "nixos";
+    firewall = {
+      enable = true;
+      trustedInterfaces = [
+        "docker0"
+      ];
+    };
     networkmanager = {
       enable = true;
       plugins = with pkgs; [
