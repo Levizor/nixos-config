a78258ade84c2df240669e71e6d05a22636e7972
Levizor <levizorri@gmail.com>
2025-09-24 00:28:33 +0200

    remove some options, use hyprland from nixpkgs

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -62,9 +62,8 @@ in
 
     hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage =
-        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
     wayfire = {
@@ -78,8 +77,8 @@ in
 
     steam = {
       enable = myopts.steam;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
+      # remotePlay.openFirewall = true;
+      # dedicatedServer.openFirewall = true;
       extraCompatPackages = with pkgs; [
         proton-ge-bin
       ];
