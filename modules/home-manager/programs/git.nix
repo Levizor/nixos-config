ce26dee2f8623ae9a6a0367cd4f6e268f0a3a03c
Levizor <levizorri@gmail.com>
2025-09-28 14:23:57 +0200

    add git hooks

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,8 +1,28 @@
+{ pkgs, lib, ... }:
 {
   programs.git = {
     enable = true;
     userName = "Levizor";
     userEmail = "levizorri@gmail.com";
+
+    hooks = {
+      post-checkout = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "post-checkout";
+          text = ''
+            dark-text --new-area -t "$(git rev-parse --abbrev-ref HEAD)" >/dev/null 2>&1 &
+          '';
+        }
+      );
+      post-commit = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "post-commit";
+          text = ''
+            dark-text -t "CODE BLESSED" >/dev/null 2>&1 &
+          '';
+        }
+      );
+    };
 
     extraConfig = {
       push = {
