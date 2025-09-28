5e6c7aeca7324f82049dbd4ff21cc4224f91896d
Levizor <levizorri@gmail.com>
2025-09-28 16:31:17 +0200

    configure btop

--- modules/home-manager/programs/btop.nix
+++ modules/home-manager/programs/btop.nix
@@ -4,6 +4,10 @@
     settings = {
       vim_keys = true;
       force_tty = true;
+      proc_tree = true;
+      proc_aggregate = true;
+      proc_sorting = "memory";
+      shown_boxes = "proc cpu mem";
     };
   };
 }
