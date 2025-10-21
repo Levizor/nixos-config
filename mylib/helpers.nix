812c01642bca2c73b588dbec5e6a6fde65ec3a98
Levizor <levizor@disroot.org>
2025-10-21 12:40:23 +0200

    split lib

---
+++ mylib/helpers.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  useModules =
+    path: names:
+    map (
+      name:
+      let
+        modulePath = path + "/${name}";
+      in
+      if builtins.pathExists (modulePath + ".nix") then modulePath + ".nix" else modulePath
+    ) names;
+
+} # helper function for loading modules with list of filenames
