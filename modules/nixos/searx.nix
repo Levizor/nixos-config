6045107d8b87a2b0812ea161518937b23399d6fa
Levizor <levizor@disroot.org>
2025-10-27 23:02:12 +0100

    update searx config

--- modules/nixos/searx.nix
+++ modules/nixos/searx.nix
@@ -7,6 +7,11 @@
         port = "8080";
         bind_address = "0.0.0.0";
         secret_key = "$SEARX_SECRET_KEY";
+        base_url = "https://${config.myopts.tailscale.address}";
+      };
+
+      search = {
+        favicon_resolver = "duckduckgo";
       };
     };
   };
