975fd225ffe045d82f5c33f19c429c34cbf74e00
Levizor <levizor@disroot.org>
2025-11-01 11:35:34 +0100

    add xdg defaults

--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -34,8 +34,18 @@
         "text/plain" = [ "nvim.desktop" ];
         "text/x-log" = [ "nvim.desktop" ];
         "text/markdown" = [ "nvim.desktop" ];
+        "x-scheme-handler/http" = [ "zen-twilight.desktop" ];
+        "x-scheme-handler/https" = [ "zen-twilight.desktop" ];
+        "x-scheme-handler/chrome" = [ "zen-twilight.desktop" ];
+        "text/html" = [ "zen-twilight.desktop" ];
+        "application/x-extension-htm" = [ "zen-twilight.desktop" ];
+        "application/x-extension-html" = [ "zen-twilight.desktop" ];
+        "application/x-extension-shtml" = [ "zen-twilight.desktop" ];
+        "application/xhtml+xml" = [ "zen-twilight.desktop" ];
+        "application/x-extension-xhtml" = [ "zen-twilight.desktop" ];
+        "application/x-extension-xht" = [ "zen-twilight.desktop" ];
       };
     };
   };
-
+  xdg.configFile."mimeapps.list".force = true;
 }
