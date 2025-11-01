436ba97c1bef85be918759f76a5361663e01d32f
Levizor <levizor@disroot.org>
2025-11-01 12:12:42 +0100

    stupidest misake ever to remove this, lol

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -68,4 +68,14 @@
   time.timeZone = "Europe/Warsaw";
 
   system.stateVersion = "25.05";
+
+  boot = {
+
+    # kernelPackages = pkgs.linuxPackages_6_1;
+
+    loader.systemd-boot.enable = true;
+
+    loader.efi.canTouchEfiVariables = true;
+
+  };
 }
