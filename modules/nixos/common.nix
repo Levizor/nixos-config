3f7b754f39b383325b1a3c2524d6290ba1e6b033
Levizor <levizor@disroot.org>
2025-12-19 00:12:01 +0100

    useless, but well

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -21,8 +21,18 @@
       "root"
       user
     ];
-    extra-substituters = [ "https://vicinae.cachix.org" ];
-    extra-trusted-public-keys = [ "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc=" ];
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://vicinae.cachix.org"
+    ];
+    trusted-substituters = [
+      "https://hyprland.cachix.org"
+      "https://vicinae.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+    ];
   };
 
   users = {
